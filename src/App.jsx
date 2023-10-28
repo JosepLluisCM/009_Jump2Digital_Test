505964bf2456b075000e9a11c33df33557caa119
JosepLluisCM <webdevJosep@gmail.com>
2023-10-28 17:45:25 +0200

    all teh requeriments met, only styling to do and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { useState } from "react";
 
 /* Import Components to Render in the App */
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import CardList from './components/CardList';
-
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import CardList from "./components/CardList";
 
 /* Style the App component here, Styled Components object replaces React Component name*/
 const AppStyled = styled.section`
@@ -18,65 +17,59 @@ const AppStyled = styled.section`
   padding: 0 5rem 0 5rem;
 `;
 
-
 function App() {
-  
-
   /* We set the states needed for the app, only the text from the input is needed, as tanQuery manages the other States*/
-  const [filterText, setFilterText] = useState('');
-
+  const [filterText, setFilterText] = useState("");
 
   /* We show more characters on scroll bottom*/
-  window.onscroll = function() {
-    const totalPageHeight = document.body.scrollHeight; 
+  window.onscroll = function () {
+    const totalPageHeight = document.body.scrollHeight;
     const scrollPoint = window.scrollY + window.innerHeight;
-    if(scrollPoint >= totalPageHeight) {
-     /*  console.log("at the bottom"); */
+    if (scrollPoint >= totalPageHeight) {
+      /*  console.log("at the bottom"); */
       fetchNextPage();
     }
-  }
+  };
 
   /* The Function to Fetch the data from API, transform into JSON DATA, and filter if FILTERTEXT is not an empty string */
   async function fetchChars({
-    pageParam = `https://rickandmortyapi.com/api/character/?page=1`
+    pageParam = `https://rickandmortyapi.com/api/character/?page=1`,
   }) {
-    const response = await fetch(pageParam + (filterText !== '' ? `&name=${filterText}` : ''));
+    const response = await fetch(
+      pageParam + (filterText !== "" ? `&name=${filterText}` : "")
+    );
     return response.json();
   }
 
   /* This is the Query Function, it manages all states and data, also refetches when needed for more characters */
-  const { data, fetchNextPage, isLoading, isError } = useInfiniteQuery({
+  const { data, fetchNextPage, isError, isLoading } = useInfiniteQuery({
     queryKey: ["characters", { filterText }],
     queryFn: fetchChars,
     getNextPageParam: (lastPage, pages) => lastPage.info?.next,
-  })
+  });
 
   /* Here we manage the info that is show on the screen depending the State of the fetch, loading, error or good, the list is shown*/
   let fetchContent = null;
-  if (isLoading) {
-    fetchContent = <p>LOADING</p>;
-  }
-  if (isError) {
-    fetchContent = <p>ERROR</p>
-  }
-  else {
+  if (data && data.pages[0].error) {
+    fetchContent = <p>No Characters with that name</p>;
+  } else {
     fetchContent = data && (
       <CardList
         list={data.pages
           .map((page) => page.results)
           .filter((results) => Boolean(results))
-          .flat()} />
-    )
+          .flat()}
+      />
+    );
   }
-
-
 
   return (
     <AppStyled>
       <Header />
       <SearchBar value={filterText} onChangeInput={setFilterText} />
+      {isLoading && <p>LOADING</p>}
+      {isError && <p>ERROR</p>}
       {fetchContent}
-      
     </AppStyled>
   );
 }
